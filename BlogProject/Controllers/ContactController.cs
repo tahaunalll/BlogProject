03fc5d007a1b9c484f183273d10ed43d5a36b3c0
tahaunalll <tahaunal@uludag.edu.tr>
2022-02-27 13:26:30 +0300

    About

---
+++ BlogProject/Controllers/ContactController.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager cm = new ContactManager(new EfContactRepository());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            //statik veri girişi yapıyoruz:
+            contact.ContactDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            contact.ContactStatus = true;
+            cm.ContactAdd(contact);
+            //Action - Controller
+            return RedirectToAction("Index","Blog");
+        }
+    }
+}
